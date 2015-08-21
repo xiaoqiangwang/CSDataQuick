41739b467117a15c373d785ad7faa108bc90220d
xiaoqiangwang <xiaoqiangwang@gmail.com>
2015-08-21 15:07:30 +0200

    test of runtime create qml window from adl

--- viewer/test.cpp
+++ viewer/test.cpp
@@ -2,7 +2,13 @@
 
 #include <QtCore/QFile>
 #include <QtDebug>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include <QQuickView>
+
 #include <fstream>
+#include <sstream>
 
 #include "common.h"
 #include "element.h"
@@ -11,8 +17,7 @@
 int main(int argc, char **argv)
 {
     std::fstream ifstream;
-    std::string filename = "/Users/wang/Development/epicsqml/tests/text.adl";
-    filename = "ADBase.adl";
+    std::string filename = argv[1];
 
     FileInfo fi = FileInfo("").getFile(filename);
     ifstream.open(fi.absolutePath().c_str(), std::fstream::in);
@@ -20,11 +25,19 @@ int main(int argc, char **argv)
     Display displayInfo(0);
     displayInfo.setFileName(fi.absolutePath());
     displayInfo.parse(ifstream);
-    displayInfo.toQML(std::cout);
 
-    std::fstream ofstream;
-    ofstream.open ("/Users/wang/Development/epicsqml/tests/main.qml", std::fstream::out);
-    displayInfo.toQML(ofstream);
+    std::ostringstream osstream;
+    displayInfo.toQML(osstream);
+    osstream.flush();
 
-    return 0;
+    QGuiApplication app(argc, argv);
+    QQmlEngine engine;
+    engine.addImportPath("..");
+
+    QQmlComponent component(&engine);
+    component.setData(osstream.str().c_str(), QUrl());
+    QWindow * window = qobject_cast<QQuickWindow *>(component.create());
+    window->show();
+
+    return app.exec();
 }
