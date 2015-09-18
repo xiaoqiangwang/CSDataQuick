69fc7f98176f2513a0e87f38cf0c5838b4d59801
xwang <xwang@localhost>
2015-09-18 21:14:49 +0200

    remove font property from base item. it is not common to all and takes resources.

--- src/PvComponents/baseitem.h
+++ src/PvComponents/baseitem.h
@@ -8,16 +8,12 @@ class BaseItem : public QQuickItem
 {
     Q_OBJECT
     Q_PROPERTY(Qt::CursorShape cursorType READ cursorType WRITE setCursorType NOTIFY cursorTypeChanged)
-    Q_PROPERTY(QFont font READ font WRITE setFont NOTIFY fontChanged)
     Q_PROPERTY(QQuickWindow* baseWindow READ baseWindow)
 public:
     explicit BaseItem(QQuickItem *parent = 0);
 
     Qt::CursorShape cursorType() {return cursor().shape();}
     void setCursorType(Qt::CursorShape cursor) {setCursor(cursor); emit cursorTypeChanged();}
-
-    QFont font() {return m_font;}
-    void setFont(const QFont font) {m_font = font; emit fontChanged();}
 
     QQuickWindow * baseWindow() {return this->window();}
 signals:
