e0e81ffd3320417872e0a50265d227d1b3fbf227
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2016-08-19 19:19:27 +0200

    refactor to consider dynamic source changes

--- src/csdatacomponents/mjpeg.cpp
+++ src/csdatacomponents/mjpeg.cpp
@@ -6,58 +6,65 @@
 
 MJPEG::MJPEG(QObject *parent) : QObject(parent), _reply(0)
 {
-    qDebug() << parent;
     _network = new QNetworkAccessManager(this);
-    connect(this, SIGNAL(destroyed(QObject*)), this, SLOT(done(QObject*)));
 }
 
 MJPEG::~MJPEG()
 {
-    printf("MJPEG deleted\n");
+    qDebug() << "MJPEG deleted";
 }
 
 void MJPEG::setSource(QString source)
 {
     if (source.isEmpty()) {
-        if (_reply)
+        if (_reply) {
+            disconnect(_reply, SIGNAL(readyRead()), this, SLOT(readStream()));
             _reply->abort();
+        }
+        _source = source;
         return;
     }
 
-    _reply = _network->get(QNetworkRequest(QUrl(source)));
-    _state = Header;
-    connect(_reply, SIGNAL(readyRead()), this, SLOT(readStream()));
+    if (source != _source) {
+        if (_reply) {
+            disconnect(_reply, SIGNAL(readyRead()), this, SLOT(readStream()));
+            _reply->abort();
+        }
+        _source = source;
+        _size = 0;
+        _state = Header;
+        _reply = _network->get(QNetworkRequest(QUrl(source)));
+        connect(_reply, SIGNAL(readyRead()), this, SLOT(readStream()));
+    }
 }
 
 void MJPEG::readStream()
 {
-    QByteArray msg;
     if (_state == Header) {
         while(_reply->canReadLine()) {
-            msg =_reply->readLine();
+            QByteArray msg =_reply->readLine();
             if (msg.isEmpty()) {
                 break;
             } else if (msg.startsWith("Content-Length: ")) {
                 _size = msg.mid(16, msg.length() - 18).toInt();
-               printf("%s", msg.constData());
-            } else if (msg == "\r\n") {
+            } else if (msg == "\r\n" && _size != 0) {
                 _state = Data;
-                printf("start data\n");
                 break;
             }
         }
-    }
-    else if (_state == Data) {
+    } else if (_state == Data) {
         if (_reply->bytesAvailable() >= _size + 2) {
             QByteArray data = _reply->read(_size);
-            QImage img;
-            if(!img.loadFromData(data)) {
-                printf("invalid data\n");
-            } else {
-                printf("got image data\n");
+
+            QByteArray msg = _reply->read(2);
+            if (msg != "\r\n") {
+                qWarning() << "Unexpected end of MJPEG data";
             }
-            _reply->readLine();
+
+            QImage img = QImage::fromData(data);
             emit imageChanged(img);
+
+            _size = 0;
             _state = Header;
         }
     }
