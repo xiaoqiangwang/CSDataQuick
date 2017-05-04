8d4be2ee225706d97d6071cff0ee59ac96f64b67
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-05-04 13:54:58 +0200

    script to create icon for 2DScan item

---
+++ tools/2dsscan.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python
+import numpy
+import Image
+
+def sscan2d():
+    x = numpy.linspace(-1, 1, 20)
+    y = numpy.linspace(-1, 1, 20)
+    xv, yv = numpy.meshgrid(x, y)
+    z = numpy.sin(numpy.sqrt(xv**2+yv**2))
+    z = numpy.exp(-(xv**2+yv**2)/0.8)
+    z = (z) * 255
+    d = numpy.zeros((25,25))
+    d[2:22,1:21] = z
+    # color scale
+    d[2:22:, 22] = numpy.linspace(255, 0, 20)
+    d[2:22:, 23] = numpy.linspace(255, 0, 20)
+
+    img = Image.fromarray(d.astype(numpy.uint8))
+    img.save('2dsscan.png', transparency=0)
+
+if __name__ == '__main__':
+    sscan2d()
