7ffc7ad7e3ad3483dfed26cba65a9dd9698bf899
XiaoqiangWang <xiaoqiangwang@gmail.com>
2017-09-07 16:13:01 +0200

    windows build batch

---
+++ conda-recipe/bld.bat
@@ -0,0 +1,14 @@
+set EPICS_BASE=%PREFIX%\epics
+if %ARCH%==32 (
+   set EPICS_HOST_ARCH=win32-x86
+) else if %ARCH%==64 (
+    set EPICS_HOST_ARCH=windows-x64
+)
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" ..
+nmake
+xcopy /E /I bin %LIBRARY_PREFIX%\bin
+xcopy /E /I plugins %LIBRARY_PREFIX%\plugins
+xcopy /E /I qml %LIBRARY_PREFIX%\qml
