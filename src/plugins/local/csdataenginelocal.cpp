bb6de653b238ac7c026216e41d74977ec0ad133f
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2018-07-03 17:10:14 +0200

    refactor

--- src/plugins/local/csdataenginelocal.cpp
+++ src/plugins/local/csdataenginelocal.cpp
@@ -36,15 +36,6 @@ QString QCSDataEngineLocal::description()
 void QCSDataEngineLocal::notifyDataChange(QCSData *data, QString name)
 {
     data->updateValue(_objectsMap[name]["value"].toVariant());
-    QString type = _objectsMap[name]["type"].toString();
-    if (type == "int")
-        data->setProperty("fieldType", QCSData::Integer);
-    else if (type == "double")
-        data->setProperty("fieldType", QCSData::Double);
-    else if (type == "enum") {
-        data->setProperty("fieldType", QCSData::Enum);
-        data->setProperty("stateStrings", _objectsMap[name]["enums"].toArray().toVariantList());
-    }
 }
 
 void QCSDataEngineLocal::create(QCSData *data)
@@ -59,8 +50,8 @@ void QCSDataEngineLocal::create(QCSData *data)
 
     // find out the root name and json object if present
     QStringList parts = source.split('.', QString::SkipEmptyParts);
+    QString rootName = parts[0];
     if (parts.length() == 1) {
-        QString rootName = parts[0];
         data->setProperty("rootName", rootName);
         // group CSData instances of the same source for convenience
         _dataMap[rootName].append(data);
@@ -69,7 +60,6 @@ void QCSDataEngineLocal::create(QCSData *data)
             notifyDataChange(data, rootName);
         }
     } else if (parts.length() == 2) {
-        QString rootName = parts[0];
         data->setProperty("rootName", rootName);
         // the supplied json object configures this source if it does not yet exits
         QJsonDocument json = QJsonDocument::fromJson(parts[1].toLocal8Bit());
@@ -90,6 +80,17 @@ void QCSDataEngineLocal::create(QCSData *data)
         return;
     }
     // update common constant properties
+    QString type = _objectsMap[rootName]["type"].toString();
+    if (type == "string")
+        data->setProperty("fieldType", QCSData::String);
+    else if (type == "int")
+        data->setProperty("fieldType", QCSData::Integer);
+    else if (type == "double")
+        data->setProperty("fieldType", QCSData::Double);
+    else if (type == "enum") {
+        data->setProperty("fieldType", QCSData::Enum);
+        data->setProperty("stateStrings", _objectsMap[rootName]["enums"].toArray().toVariantList());
+    }
     data->setProperty("count", 1);
     data->setProperty("accessRight", int(QCSData::ReadAccess | QCSData::WriteAccess));
 
@@ -128,7 +129,6 @@ void QCSDataEngineLocal::setValue(QCSData *data, const QVariant value)
     // notify the change to all data
     for(int i=0; i<_dataMap[rootName].length(); i++) {
         QCSData *d = _dataMap[rootName][i];
-        if (d != data)
         notifyDataChange(d, rootName);
     }
 }
