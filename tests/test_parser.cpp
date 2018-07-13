7a733da930f004a036a07f6dbebbe244b9ba6049
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2018-07-13 09:03:00 +0200

    generalize test for all parsers

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,11 +1,28 @@
+#include <QFileInfo>
+#include <QtDebug>
+
 #include <iostream>
+
 #include "EDLParser.h"
+#include "ADLParser.h"
+#include "UIParser.h"
 
 int main(int argc, char **argv)
 {
-    std::string filename = std::string(argv[1]);
+    QFileInfo file(argv[1]);
 
-    std::string qml = parseEDLDisplay(filename, std::map<std::string, std::string>());
+    std::string qml;
+    
+    if (file.suffix() == "adl")
+        qml = parseADLDisplay(file.absoluteFilePath().toStdString(), std::map<std::string, std::string>());
+    else if (file.suffix() == "edl")
+        qml = parseEDLDisplay(file.absoluteFilePath().toStdString(), std::map<std::string, std::string>());
+    else if (file.suffix() == "ui")
+        qml = parseUIDisplay(file.absoluteFilePath().toStdString(), std::map<std::string, std::string>());
+    else {
+        qCritical() << file.absoluteFilePath() << " is not supported";
+        return -1;
+    }
 
     std::cout << qml << std::endl;
 }
