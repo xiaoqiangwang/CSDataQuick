ad553d348baad5f7eb8a5a640e09defae314fea6
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2021-06-17 15:49:35 +0200

    fix installation directory

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,4 +1,9 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DKDE_INSTALL_BINDIR=bin \
+      -DKDE_INSTALL_LIBDIR=lib \
+      -DKDE_INSTALL_PLUGINDIR=plugins \
+      -DKDE_INSTALL_QMLDIR=qml \
+      ..
 make install
